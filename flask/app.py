7c71ec1a2aa69a15d017f1c85d9d442d6c008cdf
cmprzekop <157924419+cmprzekop@users.noreply.github.com>
2024-07-21 23:54:44 -0400

    Update app.py to include PORT variable and set port=PORT in the app.run call

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, make_response
+import os 
 
 app = Flask(__name__)
 
 @app.route('/')
+
+PORT = int(os.getenv('PORT'))
 def hello():
     response = make_response(
         {
@@ -16,4 +19,4 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port = PORT, debug=True)
